c67d7b5252628c7a9868d15a322b112a37fac895
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-09-02 16:47:08 +0530

    completed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express()
 
+app.get("/", async(req, res) => {
+
+    res.send(`<h1>Timestamp microservice</h1>Example usage:<br><a href='/api/2015-12-25' target="_blank">/api/2015-12-25</a>
+        
+        <br><a href='/api/1451001600000' target='_blank'>/api/1451001600000</a>`)
+})
+
+app.get('/api', async(req, res) => {
+    const unix = Date.now()
+    let utc = new Date().toUTCString()
+    return res.status(200).json({unix, utc})
+})
+
 app.get("/api/:date", async (req, res) => {
     const date = req.params.date;
    
@@ -23,6 +36,10 @@ app.get("/api/:date", async (req, res) => {
     return res.status(200).json({"unix": date*1, utc: unixToUTC})
 
 
+})
+
+app.all("*", async(req, res) => {
+    res.sendStatus(404)
 })
 
 const PORT = process.env.PORT || 6060
